eeca5e5ead5cb9ad38ca27d77fcc6a7eed822862
Sidarta Silva <semprebono@gmail.com>
2019-10-25 18:13:10 -0300

    Use vertx routes and parameters

--- src/main/java/com/sidartasilva/projeto001/App.java
+++ src/main/java/com/sidartasilva/projeto001/App.java
@@ -2,6 +2,8 @@ package com.sidartasilva.projeto001;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+
 
 /**
  * Hello world!
@@ -11,11 +13,16 @@ public class App extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
+        Router router = Router.router(vertx);
+        router.get("/").handler(rc -> rc.response().end(new JsonObject().put("hello", "project-001")
+            .toString()));
+        router.get("/:name").handler(rc -> rc.response().end(
+            new JsonObject().put("hello", rc.pathParam("name")).toString()));
+        
         vertx.createHttpServer()
-            .requestHandler(request -> {
-                request.response().end(new JsonObject().put("project", "001").toString());
-            })
-            .listen(8081);
+            .requestHandler(router::accept)
+            .listen(8082);
+        
     }
 
 }
