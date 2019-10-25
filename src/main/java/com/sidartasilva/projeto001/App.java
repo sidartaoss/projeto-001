1b970eb64105dc4c46642d46eedfad5154ffadb8
Sidarta Silva <semprebono@gmail.com>
2019-10-25 12:46:18 -0300

    Add vertx to application

--- src/main/java/com/sidartasilva/projeto001/App.java
+++ src/main/java/com/sidartasilva/projeto001/App.java
@@ -1,13 +1,21 @@
 package com.sidartasilva.projeto001;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer()
+            .requestHandler(request -> {
+                request.response().end(new JsonObject().put("project", "001").toString());
+            })
+            .listen(8081);
     }
+
 }
