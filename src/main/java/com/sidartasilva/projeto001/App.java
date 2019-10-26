69ddd806f9841cb2331a6a3f3b03fc63ce9d6baa
Sidarta Silva <semprebono@gmail.com>
2019-10-26 18:48:33 -0300

    Add vertx to project

--- src/main/java/com/sidartasilva/projeto001/App.java
+++ src/main/java/com/sidartasilva/projeto001/App.java
@@ -1,13 +1,22 @@
 package com.sidartasilva.projeto001;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        Router router = Router.router(vertx);
+        router.get("/").handler(rc -> rc.response().end(
+            new JsonObject().put("hello", "project-001").toString()
+        ));
     }
+
 }
+
